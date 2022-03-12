8ed2f84539bcf7e2ed6a0fb9c9c3928c2dfe2c01
Zeonx9 <mushtukov.artem@mail.ru>
2022-03-12 22:51:43 +0300

    extracting bit-codes of each character from tree done (function fillCodeTable() - use tree and fill codes for each in table)

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "tree_queue.h"
+#include <string.h>
 
 Queue process(char *fileName) {
     FILE * file = fopen(fileName, "rb");
@@ -33,11 +34,40 @@ void printTree(TreeNode *tree, int deep) {
     printTree(tree->left, deep + 1);
 }
 
+void fillCodeTable(TreeNode *tree, char **table, char *prefix, int prefixLen) {
+    if (!tree)
+        return;
+    if (!tree->left && !tree->right) { // for symbols save complete code in the table
+        if (!prefixLen) {
+            char *code = calloc(2, sizeof(char));
+            code[0] = '0';
+            table[tree->symbol] = code;
+            return;
+        }
+        char * code = malloc(prefixLen * sizeof(char));
+        strcpy(code, prefix);
+        table[tree->symbol] = code;
+        prefix[prefixLen - 1] = 0;
+        return;
+    }
+    prefix[prefixLen] = '0';
+    fillCodeTable(tree->left, table, prefix, prefixLen + 1);
+    prefix[prefixLen] = '1';
+    fillCodeTable(tree->right, table, prefix, prefixLen + 1);
+    prefix[prefixLen] = 0;
+}
+
 int main() {
     Queue q = process("test.txt");
     for (QueueNode *n = q.front; n; n = n->next)
         printf("%c - %ld\n", n->data->symbol, n->priority);
     TreeNode *tree = makeTree(&q);
     printTree(tree, 0);
+    char **codeTable = calloc(256, sizeof(char *));
+    char prefix[25] = {};
+    fillCodeTable(tree, codeTable, prefix, 0);
+    for (int i = 0; i < 256; ++i)
+        if (codeTable[i])
+            printf("%c = %s\n", (unsigned char) i, codeTable[i]);
     return 0;
 }
