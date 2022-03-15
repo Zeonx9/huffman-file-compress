148a3418696001c929e3f9f610dca54c1ea43ff5
Zeonx9 <mushtukov.artem@mail.ru>
2022-03-15 15:26:47 +0300

    1. debug methods transferred from main (will be deleted letter) 2. createPriorityQueueFromFile changed to createPriorityQueue, building counter extracted from here to getCounterOfFile, to make building Queue method more flexible and to reuse it in decoding 3. According changes processing Queue and .codeTable methods 4. fillMeta created to save metadata for decoding file 5. fillBuffer created to put encoded bytes in output buffer right after metadata 6. According changes in encode method now tree deleted within this method 7. According changes in deleteEncoder

--- encode.c
+++ encode.c
@@ -1,28 +1,46 @@
 #include "encode.h"
 #include <stdio.h>
 #include <string.h>
+#include "bits.h"
 
-// create a queue, memory is allocated for each symbol-node of tree (freed in deleteTree() )
-// also memory allocated for counter (freed in deleteEncoder() )
-Queue createPriorityQueueFromFile(Encoder *en) {
+// debug
+void printTree(TreeNode *tree, int deep) {
+    if (!tree) return;
+    printTree(tree->right, deep + 1);
+    for (int i = 0; i < deep; ++i)
+        printf("\t");
+    printf("%u(%c)\n", tree->symbol, tree->symbol);
+    printTree(tree->left, deep + 1);
+}
+void printCodeTable(CharCode * codeTable) {
+    for (int i = 0; i < 256; ++i)
+        if (codeTable[i].code)
+            printf("%c = %s\n", (unsigned char) i, codeTable[i].code);
+}
+
+// count frequency of each character within file
+void getCounterOfFile(Encoder *en) {
     FILE * file = fopen(en->fileName, "rb");
-    if (!file)
-        exit(13); // cannot open file
+    if (!file) exit(13); // cannot open file
     fseek(file, 0L, SEEK_END);
     en->fileSize = ftell(file);
     fseek(file, 0L, SEEK_SET);
+
     unsigned long * counter = calloc(256, sizeof(unsigned long));
     for (unsigned long i = 0; i < en->fileSize; ++i) // fill counter
         ++counter[(unsigned char) fgetc(file)];
     en->counter = counter;
     fclose(file);
+}
 
+// create a queue, memory is allocated for each symbol-node of tree (freed in deleteTree() )
+// also memory allocated for counter (freed in deleteEncoder() )
+Queue createPriorityQueue(const unsigned long counter[256]) {
     Queue q = {}; // empty queue
     for (int i = 0; i < 256; ++i)
         if (counter[i]) { // for every encouraged char in the text add it to queue with its frequency as a priority
             TreeNode *node = malloc(sizeof(TreeNode));
-            if (!node)
-                exit(14); // cannot allocate memory
+            if (!node) exit(14); // cannot allocate memory
             node->right = node->left = NULL; // final node
             node->symbol = (unsigned char) i;
             enqueue(&q, node, counter[i]); // put in queue
@@ -31,19 +49,20 @@ Queue createPriorityQueueFromFile(Encoder *en) {
 }
 
 // memory allocated for each code of symbol in table of codes (freed in deleteEncoder() )
-void fillCodeTable(Encoder * en, TreeNode *tree, char *prefix, int prefixLen) {
-    if (!tree)
-        return;
+void fillCodeTable(Encoder *en, TreeNode *tree, char *prefix, int prefixLen) {
+    if (!tree) return;
     if (!tree->left && !tree->right) { // for symbols save complete code in the table
         if (!prefixLen) { // in case if tree contains only 1 node
             char *code = calloc(2, sizeof(char));
             code[0] = '0';
-            en->codeTable[tree->symbol] = code;
+            en->codeTable[tree->symbol] = (CharCode) {code, 1};
             return;
         }
-        char * code = malloc(prefixLen * sizeof(char)); // allocate memory for code string
+        char * code = calloc((prefixLen + 1), sizeof(char)); // allocate memory for code string
+        if (!code) exit(40); // cannot allocate code of symbol
+
         strcpy(code, prefix); // copy prefix
-        en->codeTable[tree->symbol] = code;
+        en->codeTable[tree->symbol] = (CharCode) {code, prefixLen};
         prefix[prefixLen - 1] = 0; // clear to go back
         return;
     }
@@ -54,20 +73,101 @@ void fillCodeTable(Encoder * en, TreeNode *tree, char *prefix, int prefixLen) {
     prefix[prefixLen] = 0; // clear to go back
 }
 
-// memory for code table is allocated (freed in deleteEncoder())
-void encode(Encoder *en) {
-    en->initialQueue = createPriorityQueueFromFile(en);
-    en->tree = makeTree(&en->initialQueue);
-    char prefix[25] = "";
-    en->codeTable = calloc(256, sizeof(char *));
-    fillCodeTable(en, en->tree, prefix, 0);
+// put metadata for decode in the file buffer
+void fillMeta(Encoder *en) {
+    // first 2 byte - number of different bytes
+    // then for each ascii code 5 bytes 1 for code itself then 4 is for frequency
+    unsigned char *buffer = en->buffer;
+    unsigned long bufPos = 2;
+    ShortBytes sb; LongBytes lb;
+    sb.asShort = en->counterLen;
+    memcpy(buffer, sb.asBytes, 2);
+    for (int i = 0; i < 256; ++i)
+        if (en->counter[i]) {
+            buffer[bufPos++] = (unsigned char) i;
+            lb.asLong = en->counter[i];
+            memcpy(buffer + bufPos, lb.asBytes, 4);
+            bufPos += 4;
+        }
+    lb.asLong = en->fileSize;
+    memcpy(buffer + bufPos, lb.asBytes, 4);
+    en->bufPos = bufPos + 4;
 }
 
+// memory for buffer, temp buffer allocated (no free)
+void fillBuffer(Encoder *en) {
+    FILE *file = fopen(en->fileName, "rb");
+    if (!file) exit(20); // cannot allocate
+
+    // this buffer contains '0' & '1' string representation of encoded data
+    // then it will be interpreted as byte sequence and writen to en->buffer
+    unsigned long tempBufLen = en->fileSize > 30 ? en->fileSize : 30,
+                  bufPos = en->bufPos;
+    unsigned char *temp = calloc(tempBufLen, sizeof(unsigned char)),
+                  *buffer = en->buffer;
+    unsigned long tempPos = 0; int eof = 0;
+
+    while (!eof) {
+        // copy codes of bytes to temp buffer
+        while (tempBufLen - tempPos >= 8) { // til can potentially contain next code-string
+            // fill the buffer with string representation of codes
+            int next_ch = fgetc(file);
+            if (next_ch == EOF) { eof = 1; break; }
+
+            CharCode code = en->codeTable[next_ch];
+            memcpy(temp + tempPos, code.code, code.len); // add symbol to buffer
+            tempPos += code.len; // put pos after added code
+        }
+        // copy encoded bytes to result buffer
+        unsigned long writenPos = 0;
+        for (; writenPos + 8 <= tempPos; writenPos += 8) {
+            buffer[bufPos++] = convertStringByte(temp + writenPos);
+        }
+        memcpy(temp, temp + writenPos, tempPos - writenPos);
+        tempPos -= writenPos;
+        for (unsigned long i = tempPos; i < tempBufLen; ++i) temp[i] = 0;
+    }
+    fclose(file);
+    // write remain byte
+    if (tempPos > 0) {
+        unsigned char rem[8];
+        memcpy(rem, temp, tempPos);
+        for (int i = (int) tempPos; i < 8; ++ i) rem[i] = '0';
+        buffer[bufPos++] = convertStringByte(rem);
+    }
+    en->bufPos = bufPos;
+    free(temp);
+}
+
+// memory for code table and code lens is allocated (freed in deleteEncoder())
+void encode(Encoder *en) {
+    getCounterOfFile(en);
+    Queue queue = createPriorityQueue(en->counter);
+    en->counterLen = (short) queue.len;
+    TreeNode * tree = makeTree(&queue);
+    printTree(tree, 0);
+
+    char prefix[25] = "";
+    en->codeTable = calloc(256, sizeof(CharCode));
+    fillCodeTable(en, tree, prefix, 0);
+    deleteTree(tree);
+    printCodeTable(en->codeTable);
+
+    en->buffer = calloc(en->fileSize + (6 + 5 * en->counterLen), sizeof(unsigned char));
+    fillMeta(en);
+    fillBuffer(en);
+
+    FILE * out = fopen("output.ahf", "wb");
+    if (!out) exit(63); // cannot open such file
+    fwrite(en->buffer, 1, en->bufPos, out);
+}
+
+// free all remain memory allocated for encoder
 void deleteEncoder(Encoder * en) {
     for (int i = 0; i < 256; ++i)
-        if (en->codeTable[i])
-            free(en->codeTable[i]);
+        if (en->codeTable[i].code)
+            free(en->codeTable[i].code);
     free(en->codeTable);
     free(en->counter);
-    deleteTree(en->tree);
+    free(en->buffer);
 }
