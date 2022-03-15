672b873d3b05aa9b7f4aeaafca9f9d491bc3d6bd
Zeonx9 <mushtukov.artem@mail.ru>
2022-03-15 15:16:42 +0300

    CharCode struct created to represent Code of character (wrap of char array) Changes in Encoder struct now it doesn't contain tree and queue, introduced CharCode array instead of two arrays of char * and int

--- encode.h
+++ encode.h
@@ -2,24 +2,31 @@
 #define HUFFMAN_FILES_ENCODE_H
 #include "tree_queue.h"
 
-// it's useless yet
-typedef struct {
+// container for new string-code of
+typedef struct char_code_ {
+    char *code;
+    int len;
+}CharCode;
+
+// main object of encode interface
+typedef struct encoder_ {
+    // initial data
     char fileName[100];
     unsigned long fileSize;
+    // counted by according methods
     unsigned long * counter;
-    Queue initialQueue;
-    TreeNode * tree;
-    char ** codeTable;
+    short counterLen;
+    CharCode * codeTable;
+    // needed to create output file
+    unsigned char * buffer;
+    unsigned long bufPos;
 } Encoder;
 
 /// method to encode all file (just combine all steps together)
 void encode(Encoder * en);
 
 /// open given file count frequency of each character and return queue where frequency is a priority
-Queue createPriorityQueueFromFile(Encoder *en);
-
-/// by given Huffman tree fill table of new shorter code which satisfy fano condition
-void fillCodeTable(Encoder *en, TreeNode * tree, char *prefix, int prefixLen);
+Queue createPriorityQueue(const unsigned long counter[256]);
 
 /// method to free spaces taken by encoder
 void deleteEncoder(Encoder * en);
