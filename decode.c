578b4f9685037b3300af2d85b9a6d5095261b057
Anton Shadrin <minecrafter.yt@ya.ru>
2022-03-16 15:42:27 +0300

    finished recoverCounter func and added some comments

--- decode.c
+++ decode.c
@@ -7,12 +7,14 @@ void recoverCounter(Decoder *dec){
     FILE* file = fopen(dec->fileName, "rb");
     if (!file) exit(17); // no such file
 
-    // size contain a real count of blocks of symbols to read
+    // counter size contain a real count of blocks of symbols to read
     ShortBytes sb;
     for (int i = 0; i < 2; ++i)
         sb.asBytes[i] = (unsigned char) fgetc(file);
     dec->counterSize = sb.asShort;
 
+    // each block of encoded file represents 5 bytes
+    // 1st - ascii code of coding byte; 2-5th - freq of this code
     LongBytes lb;
     for (int i = 0; i < dec->counterSize; ++i) {
         int asciiCode = fgetc(file);
@@ -20,4 +22,10 @@ void recoverCounter(Decoder *dec){
             lb.asBytes[j] = (unsigned char) fgetc(file);
         dec->counter[asciiCode] = lb.asLong;
     }
+
+    // last 4 bytes contain unsigned long length of origin file
+    for (int i = 0; i < 4; ++i) {
+        lb.asBytes[i] = (unsigned char) fgetc(file);
+    }
+    dec->inpFileSize = lb.asLong;
 }
--- decode.h
+++ decode.h
@@ -5,6 +5,7 @@ typedef struct decoder{
     char fileName[100];
     unsigned long counter[256];
     unsigned short int counterSize;
+    unsigned long int inpFileSize;
 }Decoder;
 
 void recoverCounter(Decoder *dec);
