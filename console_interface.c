37644d3332f49a4e64d83127ff14c7ae4ff07a56
Zeonx9 <mushtukov.artem@mail.ru>
2022-03-22 11:06:05 +0300

    realisation of console interface, recursive function to process commands, finish on "exit", or if error occurred, else will recall itself, can compress and decompress files, all needed instructions provided

---
+++ console_interface.c
@@ -0,0 +1,58 @@
+#include "console_interface.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "encode.h"
+#include "decode.h"
+
+void runConsoleFileCompression() {
+    printf("--------------------------------------------------------------------\n");
+    printf("Enter a command (compress, decompress, exit) and follow instructions\n=>");
+    char command[100] = "", inName[100] = "", outName[100] = "";
+    scanf("%s", command);
+    if (strcmp(command, "compress") == 0) {
+        printf("specify path to file to compress\n=>");
+        scanf("%s", inName);
+        printf("specify directory and name (without extension, it (.aahf) will be added automatically) of output file\n=>");
+        scanf("%s", outName);
+
+        Encoder en = {};
+        strcpy(en.fileName, inName);
+        strcpy(en.outName, outName);
+
+        clock_t begin = clock();
+            encode(&en);
+        clock_t end = clock();
+        printf("file has been compressed, in %.3lf sec.\n", (double) (end - begin) / CLOCKS_PER_SEC );
+        printf("total compression is about %.2lf%%\n", (double) (en.fileSize - en.bufPos) / en.fileSize * 100);
+        deleteEncoder(&en);
+        runConsoleFileCompression();
+    }
+    else if (strcmp(command, "decompress") == 0) {
+        printf("specify path to compressed file (.aahf), name should not include this extension)\n=>");
+        scanf("%s", inName);
+        printf("specify directory and name, where you want decompressed file to be\n"
+               "(also without extension, it will be recovered automatically)\n=>");
+        scanf("%s", outName);
+
+        Decoder dec = {};
+        strcpy(dec.fileName, inName);
+        strcpy(dec.outName, outName);
+
+        clock_t begin = clock();
+            decode(&dec);
+        clock_t end = clock();
+        printf("File has been decompressed, in %.3lf sec.\n", (double)(end - begin) / CLOCKS_PER_SEC );
+        deleteDecoder(&dec);
+        runConsoleFileCompression();
+    }
+
+    else if (strcmp(command, "exit") == 0)
+        return;
+    else {
+        printf("(!) your command can not be interpreted, only \"compress\", \"decompress\" and \"exit\" are possible!\n");
+        printf("try again.\n");
+        runConsoleFileCompression();
+    }
+
+}
