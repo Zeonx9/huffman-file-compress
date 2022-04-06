b4aea1b5bb08c3d5041d49aaae4f5ea6417fed3b
Zeonx9 <mushtukov.artem@mail.ru>
2022-04-06 12:33:58 +0300

    bits now set by substructing and adding '0'

--- bits.c
+++ bits.c
@@ -2,26 +2,26 @@
 
 unsigned char convertStringByte(const unsigned char seq[8]) {
     CharBit converter = {};
-    converter.bits.b1 = seq[0] == '1';
-    converter.bits.b2 = seq[1] == '1';
-    converter.bits.b3 = seq[2] == '1';
-    converter.bits.b4 = seq[3] == '1';
-    converter.bits.b5 = seq[4] == '1';
-    converter.bits.b6 = seq[5] == '1';
-    converter.bits.b7 = seq[6] == '1';
-    converter.bits.b8 = seq[7] == '1';
+    converter.bits.b1 = seq[0] - '0';
+    converter.bits.b2 = seq[1] - '0';
+    converter.bits.b3 = seq[2] - '0';
+    converter.bits.b4 = seq[3] - '0';
+    converter.bits.b5 = seq[4] - '0';
+    converter.bits.b6 = seq[5] - '0';
+    converter.bits.b7 = seq[6] - '0';
+    converter.bits.b8 = seq[7] - '0';
     return converter.ch;
 }
 
 void convertByteString(unsigned char *buff, const unsigned char byte){
     CharBit converter = {byte};
-    buff[0] = converter.bits.b1 ? '1' : '0';
-    buff[1] = converter.bits.b2 ? '1' : '0';
-    buff[2] = converter.bits.b3 ? '1' : '0';
-    buff[3] = converter.bits.b4 ? '1' : '0';
-    buff[4] = converter.bits.b5 ? '1' : '0';
-    buff[5] = converter.bits.b6 ? '1' : '0';
-    buff[6] = converter.bits.b7 ? '1' : '0';
-    buff[7] = converter.bits.b8 ? '1' : '0';
+    buff[0] = converter.bits.b1 + '0';
+    buff[1] = converter.bits.b2 + '0';
+    buff[2] = converter.bits.b3 + '0';
+    buff[3] = converter.bits.b4 + '0';
+    buff[4] = converter.bits.b5 + '0';
+    buff[5] = converter.bits.b6 + '0';
+    buff[6] = converter.bits.b7 + '0';
+    buff[7] = converter.bits.b8 + '0';
 }
 
